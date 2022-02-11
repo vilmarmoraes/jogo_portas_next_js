f9d5ab36466db4b750eefb3f68cf9489d56dddaf
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-02-11 16:05:19 -0300

    Projeto criado até o componente porta

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../components/Porta";
+import Presente from "../components/Presente";
+
+export default function Home() {
+  return (
+  <div>
+    {/* <Presente/> */}
+    <Porta/>
+  </div>
+  )
+}
