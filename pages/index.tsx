c2269a863a658e347b4a6f5a99053b1234a1dc6d
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-02-14 21:48:05 -0300

    Criado parametros de navegaçao e formulario inicial do jogo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-export default function Home() {
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao";
+import Link from "next/link";
+
+export default function Formulario() {
 
   return (
-    <div style={{display: "flex"}}>
-      <h1>Formulário de Inicio!!!</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c"> 
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>        
+      </div>
     </div>
   )
   
